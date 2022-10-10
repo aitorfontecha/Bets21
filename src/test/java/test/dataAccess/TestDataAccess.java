74f3bbfc5702bca7ed2f569750fa2f8f2ba893c0
gilillo32 <inigogil@bilbokoeskolapioak.org>
2022-10-10 11:57:40 +0200

    String refacto

--- src/test/java/test/dataAccess/TestDataAccess.java
+++ src/test/java/test/dataAccess/TestDataAccess.java
@@ -12,7 +12,6 @@ import javax.persistence.Persistence;
 
 import configuration.ConfigXML;
 import domain.*;
-import jdk.jfr.Registered;
 
 public class TestDataAccess {
 	protected EntityManager db;
